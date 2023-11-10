b74d6bca5f5865010b98079cc4de1b669265c0e2
Kaurcode <kaurkivi@gmail.com>
2023-11-10 12:02:38 +0200

    Äss on 11

--- MustJaak.py
+++ MustJaak.py
@@ -7,7 +7,7 @@ mastid = ("Ruu", "Ärt", "Pot", "Ris")
 kaardid = tuple(mast + ühik for mast in mastid for ühik in ühikud)
 
 #Määrab igale kaardile väärtuse
-väärtused = (2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 10) * 4 #4 erinevat masti, 10, J, Q, K, A sama väärtusega
+väärtused = (2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11) * 4 #4 erinevat masti, 10, J, Q, K sama väärtusega, A on praegu 11
 KaardiVäärtus = {kaart: väärtus for kaart, väärtus in zip(kaardid, väärtused)}
 
 #----------------
