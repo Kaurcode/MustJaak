f03f98731f49b2b18187df4deb5bf20ea8a90e58
Samuel Kolsar <samuelko@ut.ee>
2023-11-15 21:17:30 +0200

    Tkinteri testimine, mänu logo

---
+++ Tkinter_proov.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+from tkinter import *
+from PIL import ImageTk
+from PIL import Image
+
+def Mustjaak():
+    root = tk.Tk()
+
+    ikoon = Image.open("kaardid/logo.png")
+    iconPhoto = ImageTk.PhotoImage(ikoon)
+    root.title(string = "MustJaak")
+    root.iconphoto(False, iconPhoto)
+    button = Button(root, text = "Alusta mängu!", bd=5,
+                    command = root.destroy)
+    button.pack(side="bottom")
+
+    root.mainloop()
+Mustjaak()
