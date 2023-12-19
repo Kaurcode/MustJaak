1938a4b2b91c18259efb507f52e0e4b524e1c7ed
Kaurcode <kaurkivi@gmail.com>
2023-12-19 22:14:24 +0200

    Žetoonide menüü lisatud

--- MustJaak.py
+++ MustJaak.py
@@ -14,43 +14,56 @@ class Kaardipakk:
         # Loob kaardid ühe kaardipaki jaoks
         mastid = ("Ruutu", "Ärtu", "Poti", "Risti")
         ühikud = ("2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A")
-        üks_pakk = [mast + " " + ühik for mast in mastid for ühik in ühikud]
+        self.üks_pakk = [mast + " " + ühik for mast in mastid for ühik in ühikud]
 
         # Määrab igale kaardile väärtuse
         väärtused = (2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11) * 4  # 4 erinevat masti, 10, J, Q, K sama väärtusega
-        self.KaardiVäärtus = {kaart: väärtus for kaart, väärtus in zip(üks_pakk, väärtused)}
+        self.KaardiVäärtus = {kaart: väärtus for kaart, väärtus in zip(self.üks_pakk, väärtused)}
 
-        # Määratud pikkusega kaardipakk, segab paki ära
-        self.kaardipakk = pakikogus * üks_pakk
-        shuffle(self.kaardipakk)
-        self.kaardihulk = pakikogus * 52  # Kaartide kogus, et ei peaks koguaeg len()-i välja kutsuma
-        self.vahekaart = randint(round(self.kaardihulk * 0.4), round(self.kaardihulk * 0.6))  # Millal uuesti segatakse?
+        # Määratud pikkusega kaardipakk
+        self.pakikogus = pakikogus
+        self.kaardipakk = None
+        self.kaardihulk = None
+        self.vahekaart = None
+        self.sega()
 
         kaardipildid = {kaart: Image.open(f"Kaardid/{osad[0][:3] + osad[1]}.png").resize((50, 73)) for kaart, osad in
-                        [(kaart, kaart.split()) for kaart in üks_pakk]}
+                        [(kaart, kaart.split()) for kaart in self.üks_pakk]}
         self.kaardipildid = {kaart: ImageTk.PhotoImage(pilt) for kaart, pilt in kaardipildid.items()}
 
     # Kaardipakist võetakse uus kaart
     def hit(self):
+        if self.kaardihulk == 0:
+            self.sega()
         self.kaardihulk -= 1
         self.kaart = self.kaardipakk.pop(0)
         # self.kaart = "Ruutu A"  # Testimiseks
         return self.kaart, self.KaardiVäärtus[self.kaart]
 
+    def sega(self):
+        # segab paki ära
+        self.kaardipakk = self.pakikogus * self.üks_pakk
+        shuffle(self.kaardipakk)
+        self.kaardihulk = self.pakikogus * 52  # Kaartide kogus, et ei peaks koguaeg len()-i välja kutsuma
+        self.vahekaart = randint(round(self.kaardihulk * 0.4), round(self.kaardihulk * 0.6))  # Millal uuesti segatakse?
+
 
 # Igal mängijal oma käsi(kaardid) ning load, mida ta teha võib
 class Mängija:
-    def __init__(self, nimi, kaardipakk, inimene=0, ülemkäsi=None):
+    def __init__(self, nimi, kaardipakk, žetoonid=500, inimene=0, ülemkäsi=None, split=0):
         self.inimene = inimene
         self.ülemkäsi = ülemkäsi
 
         self.nimi = nimi
         self.TKnimi = tk.StringVar(value=nimi)
+        self.žetoonid = žetoonid
 
         self.kaardid = []  # Käesolevad kaardid
         self.TKkaardid = tk.StringVar()
         self.kaardid_pildiga = None
 
+        self.split_arv = split  # Mitu korda on tehtud splitti?
+        self.blackjack = 0  # Kas kaardid teevad kokku blackjack-i?
         self.väärtus = 0  # Kaartide väärtus kokku
         self.TKväärtus = tk.IntVar(value=0)
 
@@ -85,6 +98,8 @@ class Mängija:
         self.TKkaardid.set("")
         self.kaardid_pildiga = None
 
+        self.split_arv = 0  # Mitu korda on tehtud splitti?
+        self.blackjack = 0  # Kas kaardid teevad kokku blackjack-i?
         self.väärtus = 0  # Kaartide väärtus kokku
         self.TKväärtus.set(0)
 
@@ -110,6 +125,7 @@ class Mängija:
     def uuskaart(self, kaartväärtus):
         self.kaart, self.kaardiväärtus = kaartväärtus  # Kaart ise ja selle väärtus
         self.kaardid.append(self.kaart)
+        self.väärtus += self.kaardiväärtus
         self.tiitel = self.kaart.split()[1]
 
         self.kaardidarv += 1
@@ -117,13 +133,14 @@ class Mängija:
             self.luba_double = 1  # Lubatakse double, kui on ainult kaks kaarti
             self.luba_surr = 1
             # Lubatakse split, kui on kaks kaarti, mis on võrdsed
-            self.luba_split = 1 if self.väärtus == self.kaardiväärtus else 0
+            self.luba_split = 1 if self.väärtus / 2 == self.kaardiväärtus else 0
+            if self.split_arv == 0 and self.väärtus == 21:
+                self.blackjack = 1
+                self.TKvalik.set("Blackjack")
         elif self.kaardidarv == 3:
             self.luba_double = 0
             self.luba_surr = 0
             self.luba_split = 0
-
-        self.väärtus += self.kaardiväärtus
 
         # Äss saab olla, kas 1 või 11, ehk kui on väärtus üle 21, siis loetakse äss üheks
         self.A11 += 1 if self.tiitel == "A" else 0
@@ -144,6 +161,9 @@ class Mängija:
 
     def uus_pime_kaart(self, kaartväärtus):
         self.pime_kaart, self.pimekaardi_väärtus = kaartväärtus
+
+    def näita_pime_kaart(self):
+        self.uuskaart((self.pime_kaart, self.pimekaardi_väärtus))
 
     def split(self):
         self.väärtus -= self.kaardiväärtus  # Käe väärtus väheneb eelneva kaardi väärtuse võrra
@@ -326,12 +346,27 @@ class MustJaak:
 
             self.mäng()
 
+        def liida_žetoone(väärtus, mängija):
+            algne = int(mängija["žetoonid"][väärtus].get())
+            mängija["žetoonid"][väärtus].set(str(algne + 1))
+            algne_kokku = int(mängija["žetoonid"]["kokku"].get())
+            mängija["žetoonid"]["kokku"].set(str(algne_kokku + väärtus))
+
+        def lahuta_žetoone(väärtus, mängija):
+            algne = int(mängija["žetoonid"][väärtus].get())
+            mängija["žetoonid"][väärtus].set(str(algne - 1))
+            algne_kokku = int(mängija["žetoonid"]["kokku"].get())
+            mängija["žetoonid"]["kokku"].set(str(algne_kokku - väärtus))
+
         self.aken = ttk.Frame(self.root)
 
         mängijad_tabel = ttk.Frame(self.aken)
 
         p = ["p1", "p2", "p3", "p4", "p5", "p6", "p7"]
-        p_valikud = {nimi: {"olek": tk.IntVar(value=0), "inimene": tk.IntVar(value=0), "nimi": tk.StringVar()}
+        p_valikud = {nimi: {"olek": tk.IntVar(value=0), "inimene": tk.IntVar(value=0), "nimi": tk.StringVar(),
+                            "žetoonid": {"kokku": tk.StringVar(value="1875"), 1: tk.StringVar(value="25"),
+                                         5: tk.StringVar(value="20"), 25: tk.StringVar(value="10"),
+                                         100: tk.StringVar(value="5"), 500: tk.StringVar(value="2")}}
                      for nimi in p}
         i = 0
         for valik in p_valikud.values():
@@ -350,6 +385,31 @@ class MustJaak:
             nimi_väli.insert(tk.END, f"Mängija {i + 1}")
             nimi_väli.grid(row=3, column=i * 2, columnspan=2, padx=5, pady=5)
 
+            žetoonid_valik = ttk.Frame(mängijad_tabel)
+            žetoonid_näit = ttk.Entry(žetoonid_valik, justify="center", textvariable=valik["žetoonid"]["kokku"])
+            žetoonid_näit.configure(state="readonly")
+            žetoonid_näit.grid(row=0, column=0, columnspan=3)
+
+            žetoonid = (1, 5, 25, 100, 500)
+            for j, žetoon in enumerate(žetoonid):
+                j += 1
+                žetoon_pealkiri = ttk.Label(žetoonid_valik, text=str(žetoon))
+                žetoon_nupp_m = ttk.Button(žetoonid_valik, text="-", width=3,
+                                           command=lambda väärtus=žetoon, mängija=valik:
+                                           lahuta_žetoone(väärtus, mängija))
+                žetoon_näit = ttk.Entry(žetoonid_valik, width=10, justify="center",
+                                        textvariable=valik["žetoonid"][žetoon])
+                žetoon_näit.configure(state="readonly")
+                žetoon_nupp_p = ttk.Button(žetoonid_valik, text="+", width=3,
+                                           command=lambda väärtus=žetoon, mängija=valik:
+                                           liida_žetoone(väärtus, mängija))
+
+                žetoon_pealkiri.grid(row=2 * j - 1, column=0, columnspan=3)
+                žetoon_nupp_m.grid(row=2 * j, column=0, pady=5)
+                žetoon_näit.grid(row=2 * j, column=1, pady=5)
+                žetoon_nupp_p.grid(row=2 * j, column=2, pady=5)
+            žetoonid_valik.grid(row=4, column=i * 2, columnspan=2, padx=5, pady=5)
+
             i += 1
 
         mängijad_tabel.pack()
@@ -357,7 +417,7 @@ class MustJaak:
         kaardipakid_arv_pealkiri = ttk.Label(self.aken, text="Kaardipakke:")
 
         kaardipakid_arv_valikud = [str(i) for i in range(1, 9)]
-        kaardipakid_arv_valik = tk.StringVar(value="8")
+        kaardipakid_arv_valik = tk.StringVar(value="6")
         kaardipakid_arv_menüü = ttk.Combobox(self.aken, textvariable=kaardipakid_arv_valik,
                                              values=kaardipakid_arv_valikud, state="readonly")
 
@@ -439,7 +499,7 @@ class MustJaak:
         self.diiler.väärtus_pealkiri = ttk.Label(self.aken, textvariable=self.diiler.TKväärtus)
         self.diiler.väärtus_pealkiri.grid(row=2, column=0)
 
-        self.diiler.valiku_kast = ttk.Label(self.aken, textvariable=self.diiler.TKvalik)
+        self.diiler.valiku_kast = tk.Label(self.aken, textvariable=self.diiler.TKvalik)
         self.diiler.valiku_kast.grid(row=3, column=0, padx=5, pady=5, sticky="ns")
 
         self.diiler.aktiivsuskast = tk.Canvas(self.aken, width=100, height=5)
@@ -475,7 +535,7 @@ class MustJaak:
                 käsi.väärtus_pealkiri = ttk.Label(käed_aken, textvariable=käsi.TKväärtus)
                 käsi.väärtus_pealkiri.grid(row=2, column=j, padx=5, pady=5, sticky="ns")
 
-                käsi.valiku_kast = ttk.Label(käed_aken, textvariable=käsi.TKvalik)
+                käsi.valiku_kast = tk.Label(käed_aken, textvariable=käsi.TKvalik)
                 käsi.valiku_kast.grid(row=3, column=j, padx=5, pady=5, sticky="ns")
 
                 käsi.aktiivsuskast = tk.Canvas(käed_aken, width=100, height=5)
@@ -539,6 +599,8 @@ class MustJaak:
         self.aken.mainloop()
 
     def mäng(self):
+        if self.kaardipakk.vahekaart < self.kaardipakk.kaardihulk:
+            self.kaardipakk.sega()
         self.diiler = Mängija("Diiler", self.kaardipakk)
         self.mängulaud()
         i = 0
@@ -549,25 +611,39 @@ class MustJaak:
             self.root.update()
             sleep(0.5)
             i += 1
-        self.diiler.uuskaart(self.kaardipakk.hit())  # Nähtav kaart
+            if self.KäedArv == i:
+                self.diiler.uuskaart(self.kaardipakk.hit())  # Nähtav kaart
+                self.root.update()
+                sleep(0.5)
+
         self.diiler.uus_pime_kaart(self.kaardipakk.hit())  # Peidetud kaart
         self.root.update()
         print(f"Diileri kaardid on: {self.diiler.kaardid} ning väärtus on {self.diiler.väärtus}")
-        sleep(0.5)
-        i = 0
+
+        if self.diiler.väärtus + self.diiler.pimekaardi_väärtus == 21:
+            self.diiler.aktiivne(1)
+            self.diiler.näita_pime_kaart()
+            self.diiler.valiku_kast.config(fg="red")
+            self.root.update()
+            sleep(0.5)
+            self.diiler.aktiivne()
+        else:
+            i = 0
+
         while self.KäedArv > i:
             mängija = self.käed[i]
             mängija.aktiivne(1)
             self.root.update()
             print(f"{mängija.nimi} kaardid on: {mängija.kaardid} ning väärtus on {mängija.väärtus}")
             i += 1
+            sleep(0.5)
             # nr = 1  # Antakse mängija "split" kätele (n.ö. alamkäsi)
-            while mängija.väärtus <= 21:  # Ei ole "bust"
+            while mängija.väärtus < 21:  # Ei ole "bust" või "blackjack"
                 # Päris mängija (inimene) teeb ise valikuid
                 if mängija.inimene:
                     valik = self.langeta_valik(mängija)
                 else:
-                    sleep(1)
+                    sleep(0.5)
                     valik = strateegia(mängija, self.diiler)
                     print(valik)
                 mängija.TKvalik.set(valik)
@@ -597,6 +673,8 @@ class MustJaak:
 
                     uus_mängija = self.käed[i]
                     self.mängijad[mängija.nimi].insert(self.mängijad[mängija.nimi].index(mängija) + 1, uus_mängija)
+                    for käsi in self.mängijad[mängija.nimi]:
+                        käsi.split_arv += 1
 
                     self.mängulaud()
                     uus_mängija.uuskaart(mängija.split())  # Üks mängija kaart alammängijale
@@ -617,37 +695,52 @@ class MustJaak:
             mängija.aktiivne()
             self.root.update()
 
-        self.diiler.aktiivne(1)
-        self.root.update()
-        self.diiler.uuskaart((self.diiler.pime_kaart, self.diiler.pimekaardi_väärtus))
-        print(f"Diileri kaardid on: {self.diiler.kaardid} ning väärtus on {self.diiler.väärtus}")
-        self.root.update()
-        while self.diiler.väärtus < 17 or self.diiler.väärtus == 17 and self.diiler.A11 > 0:
-            self.diiler.TKvalik.set("Hit")
-            sleep(0.5)
-            self.diiler.uuskaart(self.kaardipakk.hit())
-            self.root.update()
+        if self.diiler.kaardidarv == 1:
+            self.diiler.aktiivne(1)
+            self.diiler.näita_pime_kaart()
             print(f"Diileri kaardid on: {self.diiler.kaardid} ning väärtus on {self.diiler.väärtus}")
-        if self.diiler.väärtus > 21:
-            self.diiler.TKvalik.set("Bust")
-        else:
-            self.diiler.TKvalik.set("Stand")
-        self.diiler.aktiivne()
-        self.root.update()
-        sleep(0.5)
+            self.root.update()
+            sleep(0.5)
+            while self.diiler.väärtus < 17 or self.diiler.väärtus == 17 and self.diiler.A11 > 0:
+                self.diiler.TKvalik.set("Hit")
+                self.diiler.uuskaart(self.kaardipakk.hit())
+                self.root.update()
+                print(f"Diileri kaardid on: {self.diiler.kaardid} ning väärtus on {self.diiler.väärtus}")
+                sleep(0.5)
+            if self.diiler.väärtus > 21:
+                self.diiler.TKvalik.set("Bust")
+            else:
+                self.diiler.TKvalik.set("Stand")
+            self.diiler.aktiivne()
+            self.root.update()
+            sleep(0.5)
         for mängija in self.käed:
-            if mängija.väärtus > 21:
+            if mängija.TKvalik.get() == "Surrender":
+                mängija.valiku_kast.config(fg="red")
+            elif self.diiler.blackjack:
+                if mängija.blackjack:
+                    mängija.TKvalik.set("Push")
+                    mängija.valiku_kast.config(fg="yellow")
+                else:
+                    mängija.TKvalik.set("Loss")
+                    mängija.valiku_kast.config(fg="red")
+            elif mängija.blackjack:
+                mängija.valiku_kast.config(fg="green")
+            elif mängija.väärtus > 21:
                 mängija.TKvalik.set("Bust")
+                mängija.valiku_kast.config(fg="red")
             elif mängija.väärtus == self.diiler.väärtus:
                 mängija.TKvalik.set("Push")
-            elif mängija.väärtus == 21 and mängija.kaardidarv == 2:
-                mängija.TKvalik.set("Blackjack")
+                mängija.valiku_kast.config(fg="yellow")
             elif self.diiler.väärtus > 21:
                 mängija.TKvalik.set("Win")
+                mängija.valiku_kast.config(fg="green")
             elif mängija.väärtus > self.diiler.väärtus:
                 mängija.TKvalik.set("Win")
+                mängija.valiku_kast.config(fg="green")
             else:
                 mängija.TKvalik.set("Loss")
+                mängija.valiku_kast.config(fg="red")
             self.root.update()
             sleep(0.5)
 
